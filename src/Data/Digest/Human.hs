dc95985e17a590aa14ef72e3b26b355c203e3108
Chris Heller <hellertime@gmail.com>
2014-10-30 07:33:49 -0400

    Add PGP style human hashes

--- src/Data/Digest/Human.hs
+++ src/Data/Digest/Human.hs
@@ -1,10 +1,20 @@
-module Data.Digest.Human (humanHash) where
+module Data.Digest.Human
+    (
+      -- * Export the bsAtron based human hash
+      humanHash
+
+      -- * Export PGP word-list based human hash
+    , humanHashPGP
+    , humanHashPGP'
+    )
+  where
 
 import Data.Foldable
 import Data.Hashable
 import Data.Text     (Text)
 
 import Data.Digest.Human.BsAtron
+import Data.Digest.Human.PGP
 
 -- | Alias for 'humanHashBS'
 humanHash :: (Foldable t, Hashable a) => Int -> t a -> [Text]
